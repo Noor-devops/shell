ae22e9c49ab67e650546964c262344648f8c894d
Noor-devops <noorshanu07@gmail.com>
2026-06-15 11:57:36 +0530

    hello world script created

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -2,8 +2,8 @@
 
 ##### Special Vars #####
 echo "All variables passed to script: $@"
-# echo "Number of variables passed: $#"
-# echo "First variable: $1"
+echo "Number of variables passed: $#"
+echo "First variable: $1"
 # echo "Script name: $0"
 # echo "Who is running this: $USER"
 # echo "Which directory: $PWD"
