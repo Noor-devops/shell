59dd3f3b602d013ea80ad49dd6f8fedfff5588e0
Noor-devops <noorshanu07@gmail.com>
2026-06-15 14:34:40 +0530

    hello world script created

--- 10-logs.sh
+++ 10-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_DIR=/var/log/shell-script
+LOGS_DIR=/home/ec3-user/shell-logs
 LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
 
 # Check root access or not
