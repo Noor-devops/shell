d37e63c0a19f29a49203a22fb4736213128484e6
Noor-devops <noorshanu07@gmail.com>
2026-06-15 15:37:18 +0530

    hello world script created

--- 11-loops.sh
+++ 11-loops.sh
@@ -5,3 +5,19 @@ for number in {1..100}
 do
     echo $number
 done
+
+# loops
+# ======
+
+# for (int i=0; i<5; i++){
+
+# 	print i;
+# }
+
+# for <expression>
+# do
+# 	statements
+# done
+
+
+# script.sh mysql nginx
