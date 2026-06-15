02b6c334043bd5813fa9090f9cc0e363fbc81eb0
Noor-devops <noorshanu07@gmail.com>
2026-06-15 12:00:15 +0530

    hello world script created

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -8,11 +8,11 @@ echo "Script name: $0"
 echo "Who is running this: $USER"
 echo "Which directory: $PWD"
 echo "Home directory: $HOME"
-# echo "PID of the current script: $$"
-# sleep 5 &
-# echo "PID of the background command running just now: $!"
-# wait $!
-# echo "Line number: $LINENO"
-# echo "Script executed in $SECONDS seconds"
-# echo "Random number: $RANDOM"
-# echo "Exit code of previous command: $?"
+echo "PID of the current script: $$"
+sleep 5 &
+echo "PID of the background command running just now: $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random number: $RANDOM"
+echo "Exit code of previous command: $?"
