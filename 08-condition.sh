8fe71ff94a0aa7f2ebf0510a30d8807e2a046ac7
Noor-devops <noorshanu07@gmail.com>
2026-06-15 12:25:18 +0530

    hello world script created

--- 08-condition.sh
+++ 08-condition.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# NUMBER=$1
+NUMBER=$1
 
-# # gt - greater than
-# # lt - less than
-# # eq - equal
-# # ne - not equal
-# # ge - greater than or equal
-# # le - less than or equal
+# gt - greater than
+# lt - less than
+# eq - equal
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal
 
-# if [ $NUMBER -gt 20 ]; then
-#     echo "Given number $NUMBER is greater to 20"
-# elif [ $NUMBER -eq 20 ]; then
-#     echo "Given number $NUMBER is equal to 20"
-# else
-#     echo "Given number $NUMBER is less than 20"
-# fi
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number $NUMBER is greater to 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number $NUMBER is equal to 20"
+else
+    echo "Given number $NUMBER is less than 20"
+fi
+
+sleep 5 &
+echo "PID of the background command running just now: $!"
+wait $!
 
 num1=$1
 num2=$2
