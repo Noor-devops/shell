c20dea8c032baf4a0286f5f2695c1eb9561f8b08
Noor-devops <noorshanu07@gmail.com>
2026-06-15 12:16:03 +0530

    hello world script created

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -9,7 +9,7 @@ echo "Sum is: $SUM"
 
 # Array
 MOVIES=("RRR" "Varanasi" "Pushpa") # index always starts from 0
-echo "Movies are: ${MOVIES[@]}"
+echo "Movies are: ${MOVIES[@]}" # to access all movies [@]
 echo "First movie is: ${MOVIES[0]}"
 echo "Second movie is: ${MOVIES[1]}"
 echo "Third movie is: ${MOVIES[2]}"
