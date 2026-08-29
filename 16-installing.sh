9d887a0c461ea5e1f60dcb58d8bc648c101d5eb7
Noor-devops <noorshanu07@gmail.com>
2026-08-30 00:25:19 +0530

    shell

--- 16-installing.sh
+++ 16-installing.sh
@@ -6,7 +6,14 @@ if [ $USERID -ne 0 ]; then
     exit 1 # edi manam tesukuntunna precration
 fi
 
-
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "installimg is $1 ...failed"
+        exit 1
+    else
+        echo "installing is $1 success"
+    fi
+}
 dnf list installed mysql
 
 if [ $? -eq 0 ]; then
@@ -15,13 +22,7 @@ if [ $? -eq 0 ]; then
 else
     echo "installing mysql"
     dnf install mysql -y
-
-    if [ $? -ne 0 ]; then
-        echo "installimg is ...failed"
-        exit 1
-    else
-        echo "installing is success"
-    fi
+    VALIDATE mysql $?
 fi    
 
 echo "installing nginx"
@@ -33,11 +34,5 @@ if [ $? -eq 0 ]; then
 else
     echo "installing nginx"
     dnf install nginx -y
-
-    if [ $? -ne 0 ]; then
-        echo "installimg is ...failed"
-        exit 1
-    else
-        echo "installing is success"
-    fi
+     VALIDATE nginx $?
 fi    
