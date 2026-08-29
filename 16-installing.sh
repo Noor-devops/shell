ef634a764d5fb7409bea2583d5d7ec8f6ee97204
Noor-devops <noorshanu07@gmail.com>
2026-08-29 23:59:16 +0530

    shell

--- 16-installing.sh
+++ 16-installing.sh
@@ -3,7 +3,15 @@ echo "$USERID"
 
 if [ $USERID -ne 0 ]; then
     echo "please run the script with root access"
-    exit 1
+    exit 1 # edi manam tesukuntunna precration
 fi
 
-echo "im relaxing"
+echo "installing mysql"
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "installimg is ...failed"
+    exit 1
+else
+    echo "installing is success"
+fi
