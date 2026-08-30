0001fc8a75ac1a6e823c73d21c07fce5cca285cf
Noor-devops <noorshanu07@gmail.com>
2026-08-30 12:17:21 +0530

    shell

--- 17-practice.sh
+++ 17-practice.sh
@@ -2,8 +2,8 @@ userid=$(id -u)
 echo "$userid"
 #echo "this is root user $userid"
 
-# if [ $? -ne 0 ]; then
-#     echo "please try to install with root access"
-#     exit 1
-# fi
-# echo "love"
+if [ $? -ne 0 ]; then
+    echo "please try to install with root access"
+    exit 1
+fi
+echo "love"
