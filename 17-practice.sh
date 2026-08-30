d8ed44bd23ff467530ce450461da8eec8450543a
Noor-devops <noorshanu07@gmail.com>
2026-08-30 14:16:09 +0530

    shell

--- 17-practice.sh
+++ 17-practice.sh
@@ -1,3 +1,27 @@
+# userid=$(id -u)
+# #echo "this is root user $userid"
+
+# if [ $userid -ne 0 ]; then
+#     echo "please try to install with root access"
+#     exit 1
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -eq 0 ]; then
+#     echo "mysql is already installed....SKIPPING"
+#     exit 1
+# else
+#     echo "installing mysql"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]; then
+#         echo "mysql installation failed."
+#         exit 1
+#     else 
+#         echo "mysql installation successfull"
+#     fi
+# fi
+
 userid=$(id -u)
 #echo "this is root user $userid"
 
@@ -5,6 +29,15 @@ if [ $userid -ne 0 ]; then
     echo "please try to install with root access"
     exit 1
 fi
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "$1 installation failed."
+        exit 1
+    else 
+        echo "$1 installation successfull"
+    fi
+}
 
 dnf list installed mysql
 
@@ -14,10 +47,16 @@ if [ $? -eq 0 ]; then
 else
     echo "installing mysql"
     dnf install mysql -y
-    if [ $? -ne 0 ]; then
-        echo "mysql installation failed."
-        exit 1
-    else 
-        echo "mysql installation successfull"
-    fi
+    VALIDATE mysql $?
+fi
+
+dnf list installed nginx
+
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed....SKIPPING"
+    exit 1
+else
+    echo "installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $?
 fi
