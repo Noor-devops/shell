b23af988f863b5657c8df6fcabcec5171dc8d7f1
Noor-devops <noorshanu07@gmail.com>
2026-09-02 12:07:20 +0530

    shell

--- 18-practice2.sh
+++ 18-practice2.sh
@@ -3,17 +3,18 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root access"
+    echo "$TIMESTAMP [INFO] Please run this script with root access"
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "Installing $2 is.....Failed"
+        echo "$TIMESTAMP [ERROR] Installing $2 is.....Failed"
     else
-        echo "Installing $2 is.....Success"
+        echo "$TIMESTAMP [INFO] Installing $2 is.....Success"
     fi
 }
 
