908d18e3b2a82a7f47eebc46abf5eff73b27218c
Noor-devops <noorshanu07@gmail.com>
2026-09-02 12:24:06 +0530

    shell

--- 18-practice2.sh
+++ 18-practice2.sh
@@ -4,17 +4,21 @@ USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-    echo "$TIMESTAMP [INFO] Please run this script with root access"
+    echo -e "$TIMESTAMP [INFO] $R Please run this script with root access $N"
     exit 1
 fi
 
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "$TIMESTAMP [ERROR] Installing $1 is.....Failed" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [ERROR] Installing $1 is.....$R Failed $N" | tee -a $LOGS_FILE
     else
-        echo "$TIMESTAMP [INFO] Installing $1 is.....Success" | tee -a $LOGS_FILE
+        echo "$TIMESTAMP [INFO] Installing $1 is.....$G Success $N" | tee -a $LOGS_FILE
     fi
 }
 
@@ -26,7 +30,7 @@ do
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE "Installing $package" $?
     else
-        echo "$TIMESTAMP [INFO] $package already installed ... SKIPPING"
+        echo "$TIMESTAMP [INFO] $package already installed ...$Y SKIPPING $N"
     fi
 done
 
