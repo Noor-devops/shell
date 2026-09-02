f6502fa8938ea46d29c4fb51134e58bb5f3e506b
Noor-devops <noorshanu07@gmail.com>
2026-09-02 11:58:51 +0530

    shell

--- 18-practice2.sh
+++ 18-practice2.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 USERID=$(id -u)
-# LOGS_DIR=/var/log/shell-script
-# LOGS_FILE="$LOGS_DIR/$0.log"
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
     exit 1
 fi
 
-# if [ $USERID -ne 0 ]; then
-#     echo "Please run this script with root access"
-#     exit 1
-# fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "Installing $2 is.....Failed"
+    else
+        echo "Installing $2 is.....Success"
+    fi
+}
 
 for package in $@
 do
-    echo "installing $package"
+    echo "Installing $package"
+    dnf installed $package &>> $LOGS_FILE
+    VALIDATE $? "Installing $package"
+
 done
 
 
