443a9778bf6904b4e4fd4011fb7f245388490dab
Noor-devops <noorshanu07@gmail.com>
2026-09-02 12:00:51 +0530

    shell

--- 18-practice2.sh
+++ 18-practice2.sh
@@ -20,7 +20,7 @@ VALIDATE(){
 for package in $@
 do
     echo "Installing $package"
-    dnf installed $package &>> $LOGS_FILE
+    dnf installed $package 
     VALIDATE $? "Installing $package"
 
 done
