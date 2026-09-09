ce2ede610cf7fc9d7b5c6a77234d43dc3459587c
Noor-devops <noorshanu07@gmail.com>
2026-09-09 11:53:52 +0530

    shell

--- 19-script-1.sh
+++ 19-script-1.sh
@@ -5,6 +5,6 @@ COUNTRY=INDIA
 echo "Country : $COUNTRY"
 echo "PID of script-1: $$"
 
-#sh 20-script-2.sh # it run the script but not able to access the variables
+#sh 20-script-2.sh # it run the script but not able to access the variables and PID is diiferent
 
-source ./20-script-2.sh
+source ./20-script-2.sh # here able to access the variables and PID is same.
