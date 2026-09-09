24a2ca98f01672663ab7afab046f9e5a3e9e10bf
Noor-devops <noorshanu07@gmail.com>
2026-09-09 11:52:03 +0530

    shell

--- 19-script-1.sh
+++ 19-script-1.sh
@@ -5,4 +5,6 @@ COUNTRY=INDIA
 echo "Country : $COUNTRY"
 echo "PID of script-1: $$"
 
-sh 20-script-2.sh
+#sh 20-script-2.sh # it run the script but not able to access the variables
+
+source ./20-script-2.sh
